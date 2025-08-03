958e96992cf6a0ad2007452bf231f10574286cb3
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-08-03 13:38:47 -0400

    workshop1 task 11

--- week1/workshop1.sql
+++ week1/workshop1.sql
@@ -136,4 +136,11 @@ ADD CONSTRAINT fk_orders_employees
 FOREIGN KEY (employee_id)
 REFERENCES employees (id);
 
-
+-- Task 11: enforce the one-to-many relationship between suppliers and products by adding a single ALTER TABLE statement on the products table.
+ALTER TABLE products
+ADD CONSTRAINT fk_products_categories 
+FOREIGN KEY (category_id)
+REFERENCES categories (id),
+ADD CONSTRAINT fk_products_suppliers 
+FOREIGN KEY (supplier_id)
+REFERENCES suppliers (id);
