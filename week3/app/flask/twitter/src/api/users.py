badc6590e7e8e65165d031db2f2237137d21e484
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-08-19 10:39:38 -0400

    workshop3 task 4

--- week3/app/flask/twitter/src/api/users.py
+++ week3/app/flask/twitter/src/api/users.py
@@ -1,5 +1,12 @@
 from flask import Blueprint, jsonify, abort, request
 from ..models import User, db, Tweet, likes_table
+import hashlib
+import secrets
+
+def scramble(password: str):
+    """Hash and salt the given password"""
+    salt = secrets.token_hex(16)
+    return hashlib.sha512((password + salt).encode('utf-8')).hexdigest()
 
 bp = Blueprint('users', __name__, url_prefix='/users')
 
@@ -10,3 +17,29 @@ def index():
     for u in users:
         result.append(u.serialize()) # build list of Users as dictionaries
     return jsonify(result)  # return JSON response
+
+@bp.route('/<int:id>', methods=['GET'])
+def show(id: int): # get user by id
+    u = User.query.get_or_404(id)
+    return jsonify(u.serialize())
+
+@bp.route('', methods=['POST'])
+def create():
+    # req body must contain username and password
+    if 'username' not in request.json or 'password' not in request.json:
+        return abort(400)
+    
+    # if the username value is not at least 3 characters long, or the password value is not at least 8 characters long, return with an abort of 400 as well
+    if len(request.json['username']) < 3 or len(request.json['password']) < 8:
+        return abort(400)
+
+    # construct User
+    u = User(
+        username=request.json['username'],
+        password=scramble(request.json['password'])
+    )
+
+    db.session.add(u)  # prepare CREATE statement
+    db.session.commit()  # execute CREATE statement
+
+    return jsonify(u.serialize())
