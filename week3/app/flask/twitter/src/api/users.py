9ef516f4af4eaf2aedb70dcea707aea082068295
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-08-19 10:00:54 -0400

    workshop3 task 2

--- week3/app/flask/twitter/src/api/users.py
+++ week3/app/flask/twitter/src/api/users.py
@@ -2,3 +2,11 @@ from flask import Blueprint, jsonify, abort, request
 from ..models import User, db, Tweet, likes_table
 
 bp = Blueprint('users', __name__, url_prefix='/users')
+
+@bp.route('', methods=['GET']) # decorator takes path and list of HTTP verbs
+def index():
+    users = User.query.all() # ORM performs SELECT query
+    result = []
+    for u in users:
+        result.append(u.serialize()) # build list of Users as dictionaries
+    return jsonify(result)  # return JSON response
