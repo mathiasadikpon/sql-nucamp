6e10beacdb14e5776a22c68093247da524d2a580
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-08-22 21:08:58 -0400

    week 4 update

--- week4/week4.sql
+++ week4/week4.sql
@@ -9,3 +9,10 @@ FROM moma_works
 WHERE classification = 'Photograph'
   AND width > 0
   AND height > 0;
+
+-- Set recommended frame width and height
+SELECT
+CEIL(width) + 2 AS frame_width,
+CEIL(height) + 4 AS frame_height
+FROM moma_works
+WHERE classification = 'Photograph' AND width > 0 AND height > 0;
