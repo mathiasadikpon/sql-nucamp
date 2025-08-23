26f07bd67b6b40180dcc8fe822ad497790ab1e8b
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-08-22 21:11:54 -0400

    week 4 update

--- week4/week4.sql
+++ week4/week4.sql
@@ -16,3 +16,17 @@ CEIL(width) + 2 AS frame_width,
 CEIL(height) + 4 AS frame_height
 FROM moma_works
 WHERE classification = 'Photograph' AND width > 0 AND height > 0;
+
+-- Set recommended frame area
+WITH frames AS (
+    SELECT
+    CEIL(width) + 2 AS frame_width,
+    CEIL(height) + 4 AS frame_height
+    FROM moma_works
+    WHERE classification = 'Photograph' AND width > 0 AND height > 0
+)
+SELECT
+frame_width,
+frame_height,
+frame_width * frame_height AS frame_area
+FROM frames;
