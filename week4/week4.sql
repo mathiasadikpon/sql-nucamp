f53b2a26541811712eb4e3a9cc483b3091985911
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-08-22 21:07:02 -0400

    week 4 update 2

--- week4/week4.sql
+++ week4/week4.sql
@@ -1,3 +1,11 @@
+-- Select photographs
 SELECT *
 FROM moma_works
 WHERE classification = 'Photograph';
+
+-- Ignore entries with invalid width or height
+SELECT *
+FROM moma_works
+WHERE classification = 'Photograph'
+  AND width > 0
+  AND height > 0;
