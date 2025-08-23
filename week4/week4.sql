f925ee59f9cbeae5ac44b9df94d3dcd20fcb4d1c
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-08-22 21:13:52 -0400

    week 4 update

--- week4/week4.sql
+++ week4/week4.sql
@@ -30,3 +30,19 @@ frame_width,
 frame_height,
 frame_width * frame_height AS frame_area
 FROM frames;
+
+-- Count number of frames required per size
+WITH frames AS (
+    SELECT
+    CEIL(width) + 2 AS frame_width,
+    CEIL(height) + 4 AS frame_height
+    FROM moma_works
+    WHERE classification = 'Photograph' AND width > 0 AND height > 0
+)
+SELECT
+COUNT(*),
+frame_width,
+frame_height,
+frame_width * frame_height AS frame_area
+FROM frames
+GROUP BY frame_width, frame_height, frame_area;
