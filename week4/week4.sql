1372b74b7565e9f13d7ebf3239d27bc5d643487b
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-08-22 21:34:41 -0400

    week 4 update

--- week4/week4.sql
+++ week4/week4.sql
@@ -46,3 +46,14 @@ frame_height,
 frame_width * frame_height AS frame_area
 FROM frames
 GROUP BY frame_width, frame_height, frame_area;
+
+-- Insert artist information
+INSERT INTO moma_artists (info) VALUES (
+    json_object('{display_name, Ablade Glover, nationality, Ghanaian}')
+);
+INSERT INTO moma_artists (info) VALUES (
+    json_object('{{display_name, Ablade Glover}, {nationality, Ghanaian}}')
+);
+INSERT INTO moma_artists (info) VALUES (
+    json_object('{display_name, nationality}', '{Ablade Glover, Ghanaian}')
+);
